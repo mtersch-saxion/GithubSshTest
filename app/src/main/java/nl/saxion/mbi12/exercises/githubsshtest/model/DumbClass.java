a335351fc134ea7092a03bbe66630ddf014441c4
Martijn ter Schegget <28559540+mtersch-saxion@users.noreply.github.com>
2018-04-23 16:21:44 +0200

    Some attribute with getter & setter.

--- app/src/main/java/nl/saxion/mbi12/exercises/githubsshtest/model/DumbClass.java
+++ app/src/main/java/nl/saxion/mbi12/exercises/githubsshtest/model/DumbClass.java
@@ -1,4 +1,13 @@
 package nl.saxion.mbi12.exercises.githubsshtest.model;
 
 public class DumbClass {
+    private int someNumber;
+
+    public int getSomeNumber() {
+        return someNumber;
+    }
+
+    public void setSomeNumber(int someNumber) {
+        this.someNumber = someNumber;
+    }
 }
